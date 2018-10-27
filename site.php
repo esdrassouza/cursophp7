ae5f7bf52efba62507e82cfbc6f4614d5f98847b
esdrassouza <esdrascsouza@hotmail.com>
2018-10-26 23:43:19 -0300

    ca

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -66,6 +67,19 @@ $page->setTpl("product-detail",[
    'product'=>$product->getValues(),
    'categories'=>$product->getCategories()
 ]);
+
+});
+
+
+$app->get("/cart", function(){
+   
+
+
+   $cart = Cart::getFromSession();
+
+   $page = new Page();
+
+   $page->setTpl("cart");
 
 });
 
