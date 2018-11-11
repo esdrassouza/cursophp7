d39cd479ee597fb2a456d73c125be37482ff2d4a
esdrassouza <esdrascsouza@hotmail.com>
2018-11-10 23:51:35 -0200

    Recuperando a senha

--- site.php
+++ site.php
@@ -604,4 +604,70 @@ $app->get("/profile/orders/:idorder", function($idorder){
         
 });
 
+$app->get("/profile/change-password",function(){
+
+   User::verifyLogin(false);
+
+   $page = new Page();
+   $page->setTpl("profile-change-password",[
+       
+                'changePassError'=>User::getError(),
+                'changePassSuccess'=>User::getSuccess()
+
+   ]);
+});
+
+$app->post("/profile/change-password", function(){
+      
+   User::verifyLogin(false);
+   
+   User::getFromSession();
+    
+      if(!isset($_POST['current_pass']) || $_POST['current_pass'] ==='')
+      {
+
+      	User::setError("Informe a senha atual!");
+      	header("Location: /profile/change-password");
+      	exit;
+      }
+
+       if(!isset($_POST['new_pass']) || $_POST['new_pass'] ==='')
+      {
+
+      	User::setError("Informe a nova senha!");
+      	header("Location: /profile/change-password");
+      	exit;
+      }
+
+       if(!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] ==='')
+      {
+
+      	User::setError("Confirme a nova senha!");
+      	header("Location: /profile/change-password");
+      	exit;
+      }
+
+      if($_POST['current_pass'] != $_POST['new_pass'])
+         {
+             User::setError("A sua nova senha deve ser diferente da atual!");
+      	     header("Location: /profile/change-password");
+      	     exit;
+           }
+
+           
+           if(!password_verify($_POST['current_pass'], $user->getdespassword())){
+
+           		User::setError("A senha está inválida!");
+      	        header("Location: /profile/change-password");
+      	        exit;
+           }
+
+           $user->setdespassword($_POST['new_pass']);
+           $user->update();
+
+           User::setSuccess("Senha alterada com sucesso!");
+           header("Location: /profile/change-password");
+           exit;
+});
+
 ?>
