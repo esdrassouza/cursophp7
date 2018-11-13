d20620dff35dc8db5bfcdd21226e1da3e11607c7
esdrassouza <esdrascsouza@hotmail.com>
2018-11-13 01:41:10 -0200

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -13,6 +13,12 @@ function formatPrice($vlprice){
 	return  number_format($vlprice, 2 ,",", ".");
 }
 
+function formatDate($date)
+{
+
+	return date('d/m/Y', strtotime($date));
+}
+
 function checkLogin($inadmin = true)
 {
 	return User::checkLogin($inadmin);
--- index.php
+++ index.php
@@ -16,6 +16,7 @@ require_once("admin.php");
 require_once("admin-user.php");
 require_once("admin-category.php");
 require_once("admin-products.php");
+require_once("admin-orders.php");
 
 
 
